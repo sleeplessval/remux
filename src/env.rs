54f3fbb3318ea4459d5c583e2a088d7a8b9c5fec
Valerie <sleeplessval@gmail.com>
2024-03-14 14:24:30 -0400

    corrected name of ATTACH_SYMBOL env var

--- src/env.rs
+++ src/env.rs
@@ -2,7 +2,7 @@ use std::env::var;
 
 pub type EnvVar = (&'static str, &'static str);
 
-pub static ATTACH_SYMBOL: EnvVar = ("ATTACH_SYMBOL", "*");
+pub static ATTACH_SYMBOL: EnvVar = ("REMUX_ATTACH_SYMBOL", "*");
 
 pub fn env_var(envvar: EnvVar) -> String {
 	var(envvar.0).unwrap_or(envvar.1.to_string())
